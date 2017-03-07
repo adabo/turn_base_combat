6962bedccd6bb1956ba2ada1b64a912a615e8366
Abel <Abel@KROTE>
2017-03-06 20:39:20 -0600

    3rd revision Add module2.py Add user input Remove npc

--- module1.py
+++ module1.py
@@ -1,40 +1,57 @@
-print("Game starts now!\n")
+# ====== Functions/Classes ====
 class Entity:
     def __init__(self, Name):
         self.name = Name
-        self.isMyTurn = False
+        self.is_my_turn = False
+        self.psyche_points = 0
 
-player = Entity("Abel")
-player.isMyTurn = True
-entity_objects = [player]
-
-npc = Entity("Gort")
-entity_objects.append(npc)
-
-npc = Entity("Sam")
-entity_objects.append(npc)
-
-npc = Entity("Piggy")
-entity_objects.append(npc)
-
-npc = Entity("Zax")
-entity_objects.append(npc)
-
-toggleNextEntity = False
-i = 0
-
-def setNextEntityTurn(object_list):
-    global toggleNextEntity
-
-    for entity in object_list:
-        if(entity.isMyTurn == True):
-            entity.isMyTurn = not entity.isMyTurn
-            toggleNextEntity = True
-            break
-
-
-while(i < 20):
+def setNextEntityTurn():
+    toggleNextEntity = False
     for entity in entity_objects:
-        if(entity.isMyTurn == True):
+        if(toggleNextEntity == True):
+            entity.is_my_turn = not entity.is_my_turn
+            break
+        elif(entity.is_my_turn == True):
             print("It's ", entity.name, " turn!\n")
+            entity.is_my_turn = not entity.is_my_turn
+            toggleNextEntity = True
+
+def hasPsychePoints():
+    for entity in entity_objects:
+        if(entity.psyche_points > 0):
+            return True
+# =============================
+
+# ====== Variables ============
+entity_objects = []
+i = 0
+# =============================
+
+# ====== Initialize Game ======
+print("Game starts now!\n********************************************************************************")
+u_input = ""
+while(u_input != "Y" and u_input != "y"):
+    player_count = int(input("How many players?: "))
+    print("Confirm: ", player_count, " players? (Y)es (N)o")
+    u_input = input()
+
+while(i < player_count):
+    u_input = input("Enter new player name: ")
+    entity_objects.append(Entity(u_input))
+    i += 1
+
+entity_objects[0].is_my_turn = True
+print("Round ", i + 1, " begin!")
+# ===========================
+
+# ====== Game Main ==========
+i = 0
+while(i < len(entity_objects)):
+    if(hasPsychePoints(entity_objects) == True):
+        getCombatMove("Restricted")
+    else:
+        getCombatMove("Unrestricted")
+
     setNextEntityTurn(entity_objects)
+    i += 1
+# ===========================
---
+++ module2.py
@@ -0,0 +1,79 @@
+class Entity:
+    def __init__(self, Name):
+        self.name = Name
+        self.move = ""
+        self.is_my_turn = False
+        self.psyche_points = 0
+
+def setNextEntityTurn(ObjIndex):
+    global entity_objects
+    if(ObjIndex + 1 == len(entity_objects)):    # If last object in list
+
+        entity_objects[ObjIndex].is_my_turn = False
+        entity_objects[0].is_my_turn = True
+    else:
+        entity_objects[ObjIndex].is_my_turn = False
+        entity_objects[ObjIndex + 1].is_my_turn = True
+
+def getCurrentPlayerTurn():
+    global entity_objects
+    for entity in entity_objects:
+        if(entity.is_my_turn == True):
+            return entity
+
+def getCombatMove(Restriction):
+    combat_choice = ""
+    if(Restriction == "Restricted"):
+        combat_choice = input('''
+            1. Mele
+            2. Range
+            Choice: ''')
+    elif(Restriction == "Unrestricted"):
+        combat_choice = input('''
+            1. Mele
+            2. Range
+            3. Psyche
+            Choice: ''')
+
+    if(combat_choice == '1'):
+        return "Mele"
+    elif(combat_choice == '2'):
+        return "Range"
+    elif(combat_choice == '3'):
+        return "Psyche"
+
+def hasPsychePoints(Player):
+    return Player.psyche_points > 0
+
+def resolvePsychePoints(Attacker):
+    Attacker.psyche_points -= 1
+    print(Attacker.psyche_points, " points remaining.")
+
+# --- Game Setup ---
+entity_objects = []
+i = 0
+while(i < 5):
+    u_input = input("Enter new player name: ")
+    entity_objects.append(Entity(u_input))
+    i += 1
+print("")
+
+entity_objects[0].is_my_turn = True
+
+# --- Game ---
+i = 0
+while(i < 20):
+    current_player = getCurrentPlayerTurn()
+    print("It's", current_player.name, "'s turn!")
+    if(hasPsychePoints(current_player) == True):
+        current_player.move = getCombatMove("Restricted")
+        resolvePsychePoints(current_player)
+        if(current_player.psyche_points == 0):
+            setNextEntityTurn(entity_objects.index(current_player))
+    else:
+        current_player.move = getCombatMove("Unrestricted")
+        if(current_player.move == "Psyche"):
+            current_player.psyche_points = 3
+        setNextEntityTurn(entity_objects.index(current_player))
+    print(current_player.name, "chose:", current_player.move, '\n')
+    i += 1
